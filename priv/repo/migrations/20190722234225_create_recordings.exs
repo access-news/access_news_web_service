04e7b2719f8768665f27dcd3b587f406dc07e1be
Attila Gulyas <toraritte@gmail.com>
2019-07-23 14:30:33 -0700

    Add TODO

--- priv/repo/migrations/20190722234225_create_recordings.exs
+++ priv/repo/migrations/20190722234225_create_recordings.exs
@@ -4,6 +4,11 @@ defmodule AccessNews.Repo.Migrations.CreateRecordings do
   def change do
     create table(:recordings) do
       add :recorded_at, :naive_datetime
+      # TODO What to do on user deletion?
+      #      Maybe it can be left as it is, because
+      #      we keep reader records, even if they quit
+      #      (we have to), and listeners never quit,
+      #      although it may change.
       add :user_id, references(:users, on_delete: :nothing)
 
       timestamps()
