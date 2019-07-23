43f597c2ada1ad0c3c8cded6933e06d9aff05e76
Attila Gulyas <toraritte@gmail.com>
2019-07-23 14:34:11 -0700

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -41,6 +41,13 @@ pkgs.mkShell {
     export MIX_ARCHIVES="$MIX_HOME/archives"
 
     ####################################################################
+    # Allow access to Google Cloud APIs
+    # See https://github.com/GoogleCloudPlatform/elixir-samples/tree/master/storage
+    ####################################################################
+
+    export GOOGLE_APPLICATION_CREDENTIALS=./service_account.json
+
+    ####################################################################
     # Clean up after exiting the Nix shell using `trap`.
     # ------------------------------------------------------------------
     # Idea taken from
